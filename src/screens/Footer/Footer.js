61e140d8948f479437df7d5e770909baa3f4a4d7
dev_duc <duc562000@gmail.com>
2024-04-06 15:46:46 +0700

    fix update pass

--- src/screens/Footer/Footer.js
+++ src/screens/Footer/Footer.js
@@ -1,42 +1,55 @@
-import React from 'react'
-import styles from './StyleFooter'
-import { StyleSheet, Text, View,TextInput, TouchableOpacity  } from 'react-native'
-import { AntDesign } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons'; 
-import { FontAwesome } from '@expo/vector-icons'; 
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import React from "react";
+import styles from "./StyleFooter";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 // import userAPI from '../../redux/reducers/user/userAPI';
 // import { useDispatch } from 'react-redux';
 // import tokenService from '../../services/token.service';
 
-
 function Footer(props) {
-    const navigation  = useNavigation();
-    // const dispatch = useDispatch();
-    // const token = tokenService.getAccessToken();
-    
-    const hanldPress = () =>{
-        navigation.navigate("Home");
-    }
-    //     var user = userAPI.getUserInfo()(token);
-    //     dispatch(user);
-    // }
+  const navigation = useNavigation();
+  // const dispatch = useDispatch();
+  // const token = tokenService.getAccessToken();
+
+  const hanldPress = () => {
+    navigation.navigate("Home");
+  };
+  //     var user = userAPI.getUserInfo()(token);
+  //     dispatch(user);
+  // }
   return (
     <View style={styles.container}>
-        <TouchableOpacity onPress={hanldPress} style={styles.containerIcon}>
-            <AntDesign name="message1" size={20} color="grey" />
-        </TouchableOpacity>
-        <TouchableOpacity  onPress={()=>navigation.navigate("Contacts")} style={styles.containerIcon}>
-            <FontAwesome5 name="user-friends" size={20} color="grey" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.containerIcon}>
-            <AntDesign name="clockcircleo" size={20} color="grey" />
-        </TouchableOpacity>
-        <TouchableOpacity  onPress={()=>navigation.navigate("MyProfile",{phone:props?.phone})} style={styles.containerIcon}>
-            <FontAwesome name="user" size={20} color="grey" />
-        </TouchableOpacity>
+      <TouchableOpacity onPress={hanldPress} style={styles.containerIcon}>
+        <AntDesign name="message1" size={20} color="grey" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Contacts")}
+        style={styles.containerIcon}
+      >
+        <FontAwesome5 name="user-friends" size={20} color="grey" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.containerIcon}>
+        <AntDesign name="clockcircleo" size={20} color="grey" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("MyProfile", { phone: props?.phone })
+        }
+        style={styles.containerIcon}
+      >
+        <FontAwesome name="user" size={20} color="grey" />
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
 export default Footer;
