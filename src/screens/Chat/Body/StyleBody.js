c2f6d853b44bcb744b51cde2af2e37000aa25b80
Bui Huu Dat <dat54261001@gmail.com>
2024-04-10 20:22:58 +0700

    push

--- src/screens/Chat/Body/StyleBody.js
+++ src/screens/Chat/Body/StyleBody.js
@@ -1,9 +1,9 @@
-import {StyleSheet} from 'react-native'
+import { StyleSheet } from "react-native";
 const stylesBody = StyleSheet.create({
-    container:{
-        backgroundColor:'#EEE0E5',
-        // maxHeight:'87%',
-    }
-})
+  container: {
+    backgroundColor: "#EEE0E5",
+    // maxHeight:'87%',
+  },
+});
 
 export default stylesBody;
