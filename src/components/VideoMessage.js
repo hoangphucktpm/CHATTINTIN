af540bd310be27cb5e152026cd5be52291884387
Bui Huu Dat <dat54261001@gmail.com>
2024-04-11 16:51:15 +0700

    fix video

--- src/components/VideoMessage.js
+++ src/components/VideoMessage.js
@@ -1,33 +1,22 @@
 import * as React from "react";
 import { View, StyleSheet, Button } from "react-native";
-import { Video, ResizeMode } from "expo-av";
+import { ResizeMode, Video } from "expo-av";
 
-export default function App() {
+export default function VideoMessage({ uri }) {
+  if (!uri) return null;
   const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
   return (
     <View style={styles.container}>
       <Video
         ref={video}
         style={styles.video}
         source={{
-          uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+          uri,
         }}
         useNativeControls
         resizeMode={ResizeMode.CONTAIN}
         isLooping
-        onPlaybackStatusUpdate={(status) => setStatus(() => status)}
       />
-      <View style={styles.buttons}>
-        <Button
-          title={status.isPlaying ? "Pause" : "Play"}
-          onPress={() =>
-            status.isPlaying
-              ? video.current.pauseAsync()
-              : video.current.playAsync()
-          }
-        />
-      </View>
     </View>
   );
 }
