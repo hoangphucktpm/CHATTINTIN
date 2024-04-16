498f54d4cb45a79a97bb85e46918f4d712f897e9
Huỳnh Hoàng Phúc <hoangphuchm11@gmail.com>
2024-04-16 19:57:12 +0700

    ps

--- src/screens/Login&Register/OTP.js
+++ src/screens/Login&Register/OTP.js
@@ -32,29 +32,11 @@ const OTP = (props) => {
     } else if (!regexOTP.test(otp)) {
       Alert.alert("Thông báo", "Mã OTP không hợp lệ");
     } else {
-      // sendPhoneVerification(auth.currentUser)
-      //     .then(() => {
-      //         Alert.alert("Thông báo", `Đăng ký thành công ! ${'\n'}Mời bạn kiểm tra điện thoại để xác nhận`);
-
       navigation.navigate("InputPass", { phoneNumber: phoneNumber });
-      // })
-      // .catch(error => {
-      //     Alert.alert("Thông báo", "Xảy ra lỗi! \n Mời bạn thử lại");
-      // });
+
     }
   };
-  // createUserWithPhoneAndPassword(auth,phone,passWord)
-  // .then((userCredential)=>{
-  //     var user = userCredential.user;
-  // sendPhoneVerification(user);
-  //     Alert.alert("Thông báo",`Đăng ký thành công ! ${'\n'}Mời bạn kiểm tra điện thoại để xác nhận`);
-  //     setPhone("");
-  //     setuserName("");
-  // navigation.navigate("InputPass");
-  // })
-  // .catch(error =>{
-  //     Alert.alert("Thông báo","Xảy ra lỗi! \n Mời bạn nhập lại tài khoản và mật khẩu")
-  // })
+ 
 
   return (
     <View style={styles.container}>
