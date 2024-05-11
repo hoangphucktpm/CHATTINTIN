e0aeac756a9c98616a53f1d026c00b9d7c285682
Huỳnh Hoàng Phúc <hoangphuchm11@gmail.com>
2024-05-12 00:53:32 +0700

    push

--- src/screens/Chat/Call/VideoCall.js
+++ src/screens/Chat/Call/VideoCall.js
@@ -1,48 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { View, Button } from 'react-native';
-import { mediaDevices, RTCView } from 'react-native-webrtc';
-import Peer from 'react-native-peerjs';
 
-
-const peer = new Peer();
 
 export default function VideoCall() {
   const [localStream, setLocalStream] = useState(null);
   const [remoteStream, setRemoteStream] = useState(null);
 
   useEffect(() => {
-    peer.on('call', (call) => {
-      call.answer(localStream);
-
-      call.on('stream', (stream) => {
-        setRemoteStream(stream);
-      });
+   
     });
 
-    const startLocalStream = async () => {
-      const stream = await mediaDevices.getUserMedia({
-        audio: true,
-        video: true,
-      });
-
-      setLocalStream(stream);
-    };
-
-    startLocalStream();
-  }, []);
-
+   
   const startCall = () => {
-    const call = peer.call('other-peer-id', localStream);
-
-    call.on('stream', (stream) => {
-      setRemoteStream(stream);
-    });
   };
 
   return (
     <View>
-      {localStream && <RTCView stream={localStream} />}
-      {remoteStream && <RTCView stream={remoteStream} />}
+    
       <Button title="Start Call" onPress={startCall} />
     </View>
   );
