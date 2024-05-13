e0900a6e0288586f24da81546dacebc54dd22c80
tinhtt4 <tinhtt4@vng.com.vn>
2024-05-13 09:20:15 +0700

    feat: resolve conflict & webrtc connect

--- src/screens/Chat/Call/VideoCall.js
+++ src/screens/Chat/Call/VideoCall.js
@@ -3,9 +3,18 @@ import {
   Button,
   SafeAreaView,
   View,
+  StyleSheet,
 } from 'react-native';
 
 import { RTCView, mediaDevices } from 'react-native-webrtc';
+
+const styles = StyleSheet.create({
+  rtcView: {
+    width: '100%', //dimensions.width,
+    height: '100%', //dimensions.height / 2,
+    backgroundColor: 'black',
+  },
+});
 
 const VideoCall = () => {
   const [stream, setStream] = useState(null);
@@ -14,26 +23,33 @@ const VideoCall = () => {
       let s;
       try {
         s = await mediaDevices.getUserMedia({ video: true });
+        console.log('stream started');
         setStream(s);
       } catch(e) {
         console.error(e);
       }
+    } else {
+      console.log('not start stream');
     }
   };
   return (
     <>
       <SafeAreaView>
-      {
-        stream &&
-          <RTCView
-            streamURL={stream.toURL()}
-					/>
-      }
       <View>
           <Button
             title = "Start"
             onPress = {start} />
         </View>
+      {
+        stream &&
+        <RTCView
+          key={1}
+          zOrder={2}
+          objectFit="cover"
+          style={{ ...styles.rtcView }}
+          streamURL={stream.toURL()}
+        />
+      }
       </SafeAreaView>
     </>
   );
