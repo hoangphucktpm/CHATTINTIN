2b3ac543e8be7b715d29f1caddcc23b2f852df75
tinhtt4 <tinhtt4@vng.com.vn>
2024-05-15 12:40:52 +0700

    feat: add feature video call

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { registerRootComponent } from "expo";
 
 import App from "./App";
+import { AppRegistry } from "react-native";
 
 
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+AppRegistry.registerComponent('AppTinTin', () => App);
